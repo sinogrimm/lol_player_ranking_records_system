53bd32f5fd5974a98b95cae8de1ef64b80f23856
grimm <sinogrimm@gmail.com>
2026-04-23 21:01:49 -0700

    added ranks table and data

--- DDL.sql
+++ DDL.sql
@@ -1,15 +1,27 @@
 /**
  * Names: Hunter Shipman, Rebecca Wang
+ * Group: 40
  * Assignment: Project Step 2 Draft
  * Description: Data Definition Queries and Sample Data
 
  The following queries are our own work (edit if needed).
 */
 
+SET FOREIGN_KEY_CHECKS=0;
+SET AUTOCOMMIT = 0;
+
 
 /* CREATE TABLES */
 
--- create Ranks table
+-- RW: create Ranks table
+
+DROP TABLE IF EXISTS Ranks;
+CREATE TABLE Ranks (
+    rank_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) UNIQUE NOT NULL,
+    lp_threshold INT(11) UNIQUE NOT NULL
+);
+
 
 -- ...
 
@@ -17,6 +29,26 @@
 
 /* INSERT DATA */
 
--- insert rank data
+-- RW: insert Ranks data
+
+INSERT INTO Ranks
+(title, lp_threshold)
+VALUES
+('Iron', 0),
+('Bronze', 100),
+('Silver', 200),
+('Gold', 300),
+('Platnium', 400),
+('Emerald', 500),
+('Diamond', 600),
+('Master', 700),
+('Grandmaster', 800),
+('Challenger', 900);
 
 -- ...
+
+
+
+
+SET FOREIGN_KEY_CHECKS=1;
+COMMIT;
